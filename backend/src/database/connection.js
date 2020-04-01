3a3fd44fd2428c2b0c4205555443f580d8bc78e7
Tony <tonylucasgonzalez@gmail.com>
2020-04-01 05:29:46 -0300

    backend da aplicação

---
+++ backend/src/database/connection.js
@@ -0,0 +1,9 @@
+const knex = require('knex');
+const configuration = require('../../knexfile');
+
+
+const connection = knex(configuration.development);
+
+// conexão de desenvolvimento
+
+module.exports = connection;    
